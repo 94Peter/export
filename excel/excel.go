f3a36f42bdb28a081145e4c8e6802d1fbd3f7176
Chen Yan Jui <ch.focke@gmail.com>
2024-06-11 15:50:02 +0800

    fix bug

--- excel/excel.go
+++ excel/excel.go
@@ -28,10 +28,9 @@ func (c *basic) Write(w io.Writer) error {
 		if err != nil {
 			return err
 		}
-		rowNum := 0
 		for num, rowVal := p.Next(); num != -1; num, rowVal = p.Next() {
 			for j, v := range rowVal {
-				sh.Cell(rowNum, j).SetString(fmt.Sprintf("%v", v))
+				sh.Cell(num, j).SetString(fmt.Sprintf("%v", v))
 			}
 		}
 	}
